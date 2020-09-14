ed65bc256a3aaff41b3db5fc2f352cf63597644e
ARTI1208 <leonardo906@mail.ru>
2020-09-14 19:13:04 +0300

    Updates

---
+++ precache-manifest.7a8e0fb7f535f3a59b6b0411aebd7952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43c87bf369aaeea2ef3c2e8488e0ae51",
+    "url": "/20200914/index.html"
+  },
+  {
+    "revision": "a6255a9104efe6965b64",
+    "url": "/20200914/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "a6255a9104efe6965b64",
+    "url": "/20200914/static/js/2.14e809bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/20200914/static/js/2.14e809bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a22551ab6d1b646f0b",
+    "url": "/20200914/static/js/main.f4ae7c03.chunk.js"
+  },
+  {
+    "revision": "3f7433c4b6e4e2b21a10",
+    "url": "/20200914/static/js/runtime-main.46e98709.js"
+  }
+]);
